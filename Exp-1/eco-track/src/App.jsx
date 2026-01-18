3d342a951dc2831070e5880523126f378465e8fa
Palash Mathur <mathurpalash5@gmail.com>
2026-01-18 23:45:40 +0530

    New Changes 2.0

--- Exp-1/eco-track/src/App.jsx
+++ Exp-1/eco-track/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import { HighImpact } from "./pages/logs";
 import Header from './pages/Header';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
